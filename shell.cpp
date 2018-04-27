5c8ad6cf0687b5045db12b88219b82a53462c82e
HARLI BOTT <bott@vcf0.cs.uga.edu>
2018-04-27 16:54:48 -0400

    Execvp Fixed

--- shell.cpp
+++ shell.cpp
@@ -12,100 +12,46 @@
 
 using namespace std;
 
-vector <string> args;
-bool done = false; // TODO: If command execlp is done, and command is read, delete vector elements
-
 int main(){
+
   while(true){ // program loop
     prompt(); // prompts the user 
-   
-    // takes in input and reads the whole line
-    string input;
-    getline(cin, input);
-    istringstream iss(input);
-    string word;
+  
+    char str[1042];
+    cin.getline(str, 1042);
+
+    char * tokes [1042];
+    char * token;
     int i = 0;
-    
-    while(iss >> word){ // error checking for more than 10 args
-      if(i >= 10){
-	error(1);
-      }// if
-      
-      // handles exiting
-      if(word.compare("exit") == 0 || word.compare("quit") == 0){
-	return EXIT_SUCCESS;
-      }
-      pretendMain(word); // calls the shell main to put every word into a vector
-      i++; // incraments each args
-    }// while
+    token = strtok(str, " ");
 
-    // checks for io redirection input. If not io, exec is called immediatley
-    if(io()){
-
-    }else{
-      exec();
+    if(!strcmp(token, "exit") || !strcmp(token, "quit")){
+      return EXIT_SUCCESS;
     }
-    if(done){ // handles removing vector to start over a new command string
-      breakDown();
-    }// breakdown
+
+    if(!strcmp(token, "<") || (!strcmp(token, ">")) || (!strcmp(token,">>"))){
+      io(token);
+    }
+    
+    while(token != NULL){
+
+      tokes[i] = token;
+      token = strtok(NULL, " ");
+      i++;
+    }
+
+    int pid = fork();
+    if(pid == 0){
+    execvp(tokes[0], tokes);
+    }else{
+      wait(nullptr);
+    }    
   }// shell while loop
 }// main
 
-bool io(){
-  bool returnVal = true;
-
-  if(args.at(1).compare("<") == 0){
-    cout << "<" << endl;
-  }else if(args.at(1).compare(">") == 0){
-    cout << ">" << endl;
-  }else if(args.at(1).compare(">>") == 0){
-    cout << ">>" << endl;
-  }else{
-    returnVal = false;
-  }
-  return returnVal;
-}// io redirection method
-
-void exec(){
-
-  if(args.size() == 0){
-    return;
-  }// if there is nothing in trhe vector, run loop again
+void io(char * token){
  
-  for(uint i = 0; i < args.size(); i++){
-    if(args.at(i).empty()){
-      error(2); // handles if it is trying to access a null element
-    }     
-  }// for
-  
-  // TODO : exec stuff
-  string arg1 = "/bin/";
-  arg1 += args.at(0);
-
-  const char * temp = arg1.c_str();
-  const char * flags = args.at(1).c_str(); 
-
-  // forks 
-  int pid = fork();
-  if(pid == 0){
-    // child
-    execl(temp, temp, flags, (char*)0); // handles exec   
-    done = true;  
-  }else{
-    // parent
-    wait(nullptr);
-    done = true;
-  }
-}// exec
-
-void breakDown(){
-  args.clear(); // erases all elements after command has been executed
-  done = false;
-}// shell
-
-void pretendMain(string s){
-  args.push_back(s); // loads vector up with each word in commmand stream
-}// pretend main
+}// io redirection method
 
 void prompt(){
   // gets the cwd and prints it out as a prompt to the screen
--- shell.h
+++ shell.h
@@ -1,9 +1,5 @@
 #include<string>
 
 void prompt();
-void exec();
-void pretendMain(std:: string);
-void breakDown();
 void error(int);
-bool io();
-void in();
+void io(char *);
